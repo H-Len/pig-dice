6279acfecd636b9bf70064a79a5460e4bef67947
Colton Lacey and hannah melendy <colton@>
2019-04-10 11:38:49 -0700

    add form in html, and js logic added for business and UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 // business logic for pig dice game -------
 function Game(){
   this.players = [],
-  this.currentId = 0
+  this.currentId = 0,
+  this.die = "scoredPoints";
 }
 
 
@@ -12,16 +13,46 @@ Game.prototype.addPlayer = function(){
 
 
 // business logic for player -----
-function Player(playerScore){
+function Player(playerName, playerScore){
+  this.playerName = playerName,
   this.playScore = playerScore
 }
 
+function rollDie(){
+   return Math.floor(Math.random() * 6) +1;
 
+}
 
+//business logic for dice
 
+function playDie(){
+  this.playDie = playDie;
+}
 
+console.log(Math.floor(Math.random() * 6) +1);
+console.log(rollDie());
 
+function playerTurn (playerScore) {
+  var score = 0;
+  while (player && rollDie() != 1 && playScore <= 100) {
+    function playerRoll(){
+      score += rollDie();
+  }
 
+  if ( playScore >= 100) {
+    alert("You win!");
+  }
+  return plqyerScore;
+  }
+}
 
 // User Interface for game ----
 var game = new Game();
+
+
+
+$(document).ready(function(){
+  $("form#game").submit(function(event){
+    event.preventDefault();
+  })
+})
